4e740fc0c42c9e9c83ba5d326823779f8d49f27f
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2022-04-06 01:35:55 -0400

    add firebase config

--- batch/src/main/java/com/cakecrypto/batch/service/impl/FirestoreServiceImpl.java
+++ batch/src/main/java/com/cakecrypto/batch/service/impl/FirestoreServiceImpl.java
@@ -1,6 +1,14 @@
 package com.cakecrypto.batch.service.impl;
 
 import com.cakecrypto.batch.service.FirestoreService;
+import com.google.cloud.firestore.Firestore;
 
 public class FirestoreServiceImpl implements FirestoreService {
+
+    private final Firestore db;
+
+    @Override
+    public void saveCryptoCurrency(){
+
+    }
 }
